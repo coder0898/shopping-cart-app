87497df78f2b97b70427da3b334f0cb3ac7d4512
Pratik Bakshi <bakshi.pratikcool@gmail.com>
2023-11-17 20:14:02 +0530

    reducer action for adding cart and removing done

--- src/Component/CardItem.js
+++ src/Component/CardItem.js
@@ -1,34 +1,61 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import Rating from './Rating';
+import { CartState } from '../Context/Context';
 
-const CardItem = ({ item }) => (
-    <div>
-        <Card key={item.id} style={{ width: "20rem"}}>
-            <Card.Img variant="top" src={item.imgsrc} alt="404" />
-            <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Subtitle style={{ paddingBottom: 10 }}>
-                    <span>₹ {item.price.split(".")[0]}</span>
-                    {item.fastDelivery ? (
-                        <div>Fast Delivery</div>
-                    ) : (
-                        <div>4 days delivery</div>
-                    )}
-                    <Rating rating={item.ratings} />
-                </Card.Subtitle>
-                    <Button
-                        variant="danger"
-                    >
-                        Remove from Cart
-                    </Button>
-                    <Button
-                    >
-                        {!item.inStock ? "Out of Stock" : "Add to Cart"}
-                    </Button>
-            </Card.Body>
-        </Card>
-    </div>
-)
+const CardItem = ({ item }) => {
+
+    const { state: { cart }, dispatch } = CartState();
+
+    console.log(cart);
+
+    return (
+        <div>
+            <Card key={item.id} style={{ width: "20rem" }}>
+                <Card.Img variant="top" src={item.imgsrc} alt="404" />
+                <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Subtitle style={{ paddingBottom: 10 }}>
+                        <span>₹ {item.price.split(".")[0]}</span>
+                        {item.fastDelivery ? (
+                            <div>Fast Delivery</div>
+                        ) : (
+                            <div>4 days delivery</div>
+                        )}
+                        <Rating rating={item.ratings} />
+                    </Card.Subtitle>
+                    {
+                        cart.some(p => p.id === item.id) ? (
+                            <Button
+                                variant="danger"
+                                onClick={()=>{
+                                    dispatch({
+                                        type:'REMOVE_FROM_CART',
+                                        payload:item
+                                    })                                
+                                     }}
+                            >
+                                Remove from Cart
+                            </Button>
+                        ) : (
+                            <Button
+                             onClick={()=>{
+                            dispatch({
+                                type:'ADD_TO_CART',
+                                payload:item
+                            })                                
+                             }}
+                            disabled={!item.inStock}
+                            >
+                                {!item.inStock ? "Out of Stock" : "Add to Cart"}
+                            </Button>
+                        )
+                    }
+
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
 
 export default CardItem
